27f77f70c6d8d1d3282742b5ee5a714a6d8a5f13
Pavel <pavl.romanov@gmail.com>
2025-04-21 19:42:22 +0300

    Creating test server

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import Pyro4,hashlib
-@Pyro4.expose()
+@Pyro4.expose
 class Server:
     def check(self):
         return True
 
-daemon = Pyro4.Daemon()
+daemon = Pyro4.Daemon(host="https://messanger-m8sk.onrender.com")
 uri = daemon.register(Server)
 print("uri=",uri)
 daemon.requestLoop()
